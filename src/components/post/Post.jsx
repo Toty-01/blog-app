215b2860e9d496c042a15912e5e0e27c2afa6876
Toty-01 <caucheanthony@gmail.com>
2022-12-10 01:29:02 +0100

    forty five

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,23 @@
+import './post.css'
+import imge from '../img/img (2).jpg'
+
+export default function Post() {
+  return (
+    <div className="post">
+      <img className='postImg' src={imge} alt="" />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className='Postcat'>Music </span>
+          <span className='Postcat'>Life</span>
+        </div>
+        <span className="postTitle">Lorem obcaecati quibusdam.</span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className='postDescription'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse asperiores fugit magni culpa vero nobis necessitatibus numquam facere quos amet cupiditate, ex aut aliquam nemo deleniti totam voluptas maiores libero?
+        Qui et repudiandae eveniet accusamus quos iste quo dicta itaque veniam commodi. Aspernatur quaerat ad omnis repellat, magnam commodi. Quae similique at possimus voluptatibus earum esse in labore omnis illum.
+      </p>
+    </div>
+  )
+}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,12 @@
 import "./sidebar.css"
+import img from "../img/img(5).jpg"
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebarItem">
         <span className="sidebarTitle">ABOUT ME</span>
-        <img
-          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
-          alt=""
-        />
+        <img src={img} alt="" />
         <p>
           Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
           amet ex esse.Sunt eu ut nostrud id quis proident.
